26e6237c3b7380d723295b2262acf7c4159fb33a
ThoCed <thonon_cedric@hotmail.com>
2020-04-05 11:26:11 +0200

    appel de la fonction sur l'objet camer
    la position du NodeCamera ayant bougé pour réaliser le systeme de la banane, il faut dont effectuer la rotation (rotate_x) sur l'objet Camera et plus sur le NodeCamera sinon l'effet n'est pas bon

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -23,4 +23,4 @@ func _input(event):
 			return
 		if INVERSION_MULT * SENSITIVITY_Y * event.relative.y <= 0  and self.rotation_degrees.x <= -MAX_Y:
 			return
-		rotate_x(INVERSION_MULT * SENSITIVITY_Y * event.relative.y)
+		$Camera.rotate_x(INVERSION_MULT * SENSITIVITY_Y * event.relative.y)
