fb5a1915817de814318fd61999db4aaa122ddac6
ThoCed <thonon_cedric@hotmail.com>
2020-04-14 18:49:15 +0200

    ajout asset
    ajout asset

--- Scripts/lightSDB.gd
+++ Scripts/lightSDB.gd
@@ -3,15 +3,22 @@ extends OmniLight
 var elapsedTime = 0.0
 var timeToStrobe = 0.0
 var randomGenerator
+var neonMesh:MeshInstance
+var neonMat:SpatialMaterial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomGenerator = RandomNumberGenerator.new()
+	neonMesh = get_parent().get_node("neonMesh")
+	neonMat = neonMesh.mesh.surface_get_material(0)
+	var i = 0
+	
 	
 func _process(delta):
 	elapsedTime += delta
 	if elapsedTime > timeToStrobe:
 		visible = !visible
+		neonMat.emission_enabled = !neonMat.emission_enabled
 		timeToStrobe = randomGenerator.randf_range(0.05,0.3)
 		elapsedTime = 0.0
 
